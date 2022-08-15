15a54d20eabc8a1e93d0752b858cac44351e2f08
Paolo Rossi <manuel.rossi@rappi.com>
2022-08-15 15:46:11 -0500

    add prisma

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,8 @@ import { z } from 'zod';
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  // FOO: z.string(),
+  NODE_ENV: z.enum(['development', 'test', 'production']),
+  DATABASE_URL: z.string().url(),
 });
 
 /**
